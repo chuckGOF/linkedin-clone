6c5c20c1357f3f03e9f222003021ee645f5be6df
Fadeel Gbaiye <chuck007gof@gmail.com>
2022-01-22 01:26:37 +0000

    building sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,31 @@
 import React from "react";
+import { Avatar } from "@mui/material";
 import "../styles/Sidebar.css";
 
 function Sidebar() {
 	return (
-		<div>
-			<h4 className="sidebar">this is the Sidebar</h4>
+		<div className="sidebar">
+			<div className="sidebar__top">
+				<img src="" alt="" />
+				<Avatar className="sidebar__avatar" src="" />
+				<h2>Fadeel Gbaiye</h2>
+				<h4>gbaiyefadeel@gmail.com</h4>
+			</div>
+
+			<div className="siderbar__stats">
+				<div className="sidebar_stat">
+					<p>Who viewed you</p>
+					<p className="sidebar__statNumber">2,543</p>
+					<div className="sidebar__stat">
+						<p>Views on post</p>
+						<p className="sidebar__statNumber">2,448</p>
+					</div>
+				</div>
+			</div>
+
+			<div className="sidebar__bottom">
+				<p>Recent</p>
+			</div>
 		</div>
 	);
 }
