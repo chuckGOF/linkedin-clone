a2ce0ab737fa0b0268e62b38a12042fe8bfa199d
Fadeel Gbaiye <chuck007gof@gmail.com>
2022-01-23 16:00:33 +0000

    post component

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -2,7 +2,11 @@ import React from "react";
 import "../styles/Feed.css";
 import CreateIcon from "@mui/icons-material/Create";
 import IconOption from "./IconOption";
-import ImageIcon from '@mui/icons-material/Image'
+import Post from "./Post";
+import ImageIcon from "@mui/icons-material/Image";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+import CalendarViewDayIcon from "@mui/icons-material/CalendarViewDay";
 
 function Feed() {
   return (
@@ -19,12 +23,18 @@ function Feed() {
         </div>
 
         <div className="feed__inputOption">
-          <IconOption Icon={ImageIcon}title='Photo' color='#70B5F9'/>
-          <IconOption />
-          <IconOption />
-          <IconOption />
+          <IconOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+          <IconOption Icon={SubscriptionsIcon} title="Video" color="#e7a33e" />
+          <IconOption Icon={EventNoteIcon} title="Event" color="#c0cbcd" />
+          <IconOption
+            Icon={CalendarViewDayIcon}
+            title="Write Article"
+            color="#7fc15e"
+          />
         </div>
       </div>
+
+      <Post />
     </div>
   );
 }
---
+++ src/components/Post.js
@@ -0,0 +1,36 @@
+import React, {useState} from "react";
+import "../styles/Post.css";
+import { Avatar } from "@mui/material";
+import IconOption from "./IconOption";
+import ThumbUpAltOutlinedIcon from "@mui/icons-material/ThumbUpAltOutlined";
+import ChatOutlinedIcon from "@mui/icons-material/ChatOutlined";
+import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
+import SendOutlinedIcon from "@mui/icons-material/SendOutlined";
+
+function Post({ name, description, message, photoUrl }) {
+  const [posts, setPost] = useState([])
+  return (
+    <div className="post">
+      <div className="post__header">
+        <Avatar src="" />
+        <div className="post__info">
+          <h2>Fadeel Gbaiye</h2>
+          <p>Description</p>
+        </div>
+      </div>
+
+      <div className="post__body">
+        <p>Message goes here</p>
+      </div>
+
+      <div className="post__buttons">
+        <IconOption Icon={ThumbUpAltOutlinedIcon} title="Like" color="gray" />
+        <IconOption Icon={ChatOutlinedIcon} title="Comment" color="gray" />
+        <IconOption Icon={ShareOutlinedIcon} title="Share" color="gray" />
+        <IconOption Icon={SendOutlinedIcon} title="Send" color="gray" />
+      </div>
+    </div>
+  );
+}
+
+export default Post;
